8c9b2a35f2055fe2f585ca88d24b73b225212cde
Gabe Westmaas <gabe.westmaas@rackspace.com>
2011-08-08 23:29:33 -0300

    Keeping up with API changes

--- cookbooks/newrelic/files/default/nova-api-instrumentation.py
+++ cookbooks/newrelic/files/default/nova-api-instrumentation.py
@@ -66,8 +66,8 @@ def instrument_nova_api_openstack_wsgi(module):
     # Wrap the constructor for a resource and instrument automatically
     # any controller object associated with a resource.
 
-    def in_function_resource_init(resource, controller, serializers=None,
-            deserializers=None, *args, **kwargs):
+    def in_function_resource_init(resource, controller, serializer=None,
+            deserializer=None, *args, **kwargs):
 
         newrelic.agent.log(newrelic.agent.LOG_DEBUG, str(controller))
 
@@ -82,7 +82,7 @@ def instrument_nova_api_openstack_wsgi(module):
 		    wrapped = newrelic.agent.FunctionTraceWrapper(object)
 		    setattr(controller, name, wrapped)
 
-        return ((resource, controller, serializers, deserializers), kwargs)
+        return ((resource, controller, serializer, deserializer), kwargs)
 
     newrelic.agent.wrap_in_function(module, 'Resource.__init__',
             in_function_resource_init)
